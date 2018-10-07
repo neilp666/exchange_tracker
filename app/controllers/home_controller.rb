f9212dd1eeceaf99c509b976d2ab4fd3d7c96c6f
neil patel <neil@myindianweddingapp.com>
2018-10-07 19:09:16 +0100

    Added Dotenv gem to protect API credentials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,18 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
+
+
+    #Assign App Id and base currency from Open exchange:
+    app_id = ENV['OPEN_API']
+    base = "USD"
+
+    #Grab data from Open Exchange
+    @open_url = "https://openexchangerates.org/api/latest.json?app_id=#{app_id}&base=#{base}"
+    @open_uri = URI(@open_url)
+    @open_response = Net::HTTP.get(@open_uri)
+    @open = JSON.parse(@open_response)
+
     #Grab data from coindesk
     @url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     @uri = URI(@url)
