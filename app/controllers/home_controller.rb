cf8b7da732d4f475ed3384010b42f8aabc8da5b9
neil patel <neil@myindianweddingapp.com>
2018-10-07 12:52:53 +0100

    Grabbed API data from coindesk

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
 
   def index
+    require 'net/http'
+    require 'json'
+    #Grab data from coindesk
+    @url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coindesk = JSON.parse(@response)
   end
-  
 end
